8d95fd56b7a8e284da55555c6657bdd08e6a1e23
Anwar Hasan Shuvo <shuvobhuiyan27@gmail.com>
2018-07-18 21:49:23 +0600

    Add files via upload

---
+++ Triangle/Triangle.c
@@ -0,0 +1,50 @@
+
+
+#include <GL/gl.h>
+#include <GL/glut.h>
+void display(void)
+{
+/* clear all pixels */
+glClear (GL_COLOR_BUFFER_BIT);
+/* draw white polygon (rectangle) with corners at
+* (0.25, 0.25, 0.0) and (0.75, 0.75, 0.0)
+*/
+glColor3f (255, 0, 0);
+glBegin(GL_POLYGON);
+glVertex3f (10, 15, 0.0);
+glVertex3f (20, 30, 0.0);
+glVertex3f (30, 15, 0.0);
+glEnd();
+
+
+
+glFlush ();
+}
+void init (void)
+{
+/* select clearing (background) color */
+glClearColor (0.0, 0.0, 0.0, 0.0);
+/* initialize viewing values */
+glMatrixMode(GL_PROJECTION);
+glLoadIdentity();
+glOrtho(0.0, 40.0, 0.0, 40.0, -1.0, 1.0);
+}
+/*
+* Declare initial window size, position, and display mode
+* (single buffer & RGBA). Open window with "hello"
+* In its title bar. Call initialization routines.
+* Register callback function to display graphics.
+* Enter main loop and process events.
+*/
+int main(int argc, char** argv)
+{
+glutInit(&argc, argv);
+glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+glutInitWindowSize (500, 500);
+glutInitWindowPosition (100, 100);
+glutCreateWindow ("Triangle");
+init ();
+glutDisplayFunc(display);
+glutMainLoop();
+return 0; /* ISO C requires main to return int. */
+}
